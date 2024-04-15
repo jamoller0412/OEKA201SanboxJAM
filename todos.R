4e8121d67c2f17664bda9b6f9c7d700231e35043
jamoller0412 <jamoller@hiof.no>
2024-04-15 08:16:46 +0200

    # Added todos exercises

--- todos.R
+++ todos.R
@@ -1,5 +1,18 @@
 # Todos
 ## Todo 1
+#numbers <- c(,,,)
+#sum()
 ## Todo 2
+#sqrt(c(4, 9))
+#help()
 ## Todo 3
+#?rnorm
 ## Todo 4
+# dato <- Sys.Date()
+# dato_aar <- substring(dato,1,4)
+# dato_aar_num <- as.numeric(dato_aar)
+# now <- as.numeric(substring(paste(Sys.Date()),1,4))
+# started <-
+# born <-
+# spent <- (now-started)/(now-born)*100
+# spent
